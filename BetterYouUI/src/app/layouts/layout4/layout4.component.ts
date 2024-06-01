22caa9d5abed86a2f8a449a9653343bf1e5f4011
Evan <ecook21@my.gcu.edu>
2024-06-01 14:48:17 -0500

    got graph basic working

--- BetterYouUI/src/app/layouts/layout4/layout4.component.ts
+++ BetterYouUI/src/app/layouts/layout4/layout4.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { GraphComponent } from "../../GraphComponent/graph/graph.component";
 
 @Component({
-  selector: 'app-layout4',
-  standalone: true,
-  imports: [],
-  templateUrl: './layout4.component.html',
-  styleUrl: './layout4.component.scss'
+    selector: 'app-layout4',
+    standalone: true,
+    templateUrl: './layout4.component.html',
+    styleUrl: './layout4.component.scss',
+    imports: [GraphComponent]
 })
 export class Layout4Component {
 
